16cd997925c4f9b4753aa18cfd8175cde249d134
wliqi495-create <wliqi495@gmail.com>
2026-06-14 22:42:48 +0800

    Update 2400.js

--- yingshi/js7/2400.js
+++ yingshi/js7/2400.js
@@ -1,4 +1,4 @@
 [{
   "name": "2400多部恐怖电影[恐怖电影]",
-  "url": "http://host407776532.s1169.pppf.com.cn/app/lib/2400部恐怖电影.txt&&&http://3650000.xyz/api/360.php?cid=6"
+  "url": "https://gh-proxy.org/https://raw.githubusercontent.com/wliqi495-create/jaychouqq/refs/heads/main/yingshi/txt/2400部恐怖电影.txt&&&http://3650000.xyz/api/360.php?cid=6"
     }]
